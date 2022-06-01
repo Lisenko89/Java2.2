b848f93498efab5f54379ea314469cbeaf310bb5
Алексей Лисенко <lisenko-av@bk.ru>
2022-06-01 17:53:11 +0300

    Доработка задания 2.2

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,16 @@ public class Main {
         int popolnenie = 1200;
         int bonus;
 
-        if (popolnenie > 1000) {bonus = popolnenie/100;} else {bonus = 0;};
+        if (popolnenie > 1000) {
+            bonus = popolnenie / 100;
+        } else {
+            bonus = 0;
+        }
+        ;
         int itog = schet + popolnenie + bonus;
 
-        System.out.println("У Вас на счете: " + itog+"руб.");
-        System.out.println("В том числе бонусов: " + bonus +"руб.");
+        System.out.println("У Вас на счете: " + itog + "руб.");
+        System.out.println("В том числе бонусов: " + bonus + "руб.");
 
     }
 }
