3f1831bb894786ded616fdc3fe23d95904f2a56d
Алексей Лисенко <lisenko-av@bk.ru>
2022-06-01 18:51:05 +0300

    Доработка

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int schet = 0;
-        int popolnenie = 1200;
+        int account = 0;
+        int add = 1200;
         int bonus;
 
-        if (popolnenie > 1000) {
-            bonus = popolnenie / 100;
+        if (add > 1000) {
+            bonus = add / 100;
         } else {
             bonus = 0;
         }
         ;
-        int itog = schet + popolnenie + bonus;
+        int itog = account + add + bonus;
 
         System.out.println("У Вас на счете: " + itog + "руб.");
         System.out.println("В том числе бонусов: " + bonus + "руб.");
